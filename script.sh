5c2b2a8482496d0d04f289bb88068d19a1a670df
subraban <136383571+subraban@users.noreply.github.com>
2023-07-08 17:09:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-echo Y | gcloud sql import bak instance1 gs://sqlservermedia/WideWorldImporters-Full.bak --database=db1 
+resource "null_resource" "import_backup" {
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud auth activate-service-account --key-file=creds.json
+      echo Y | gcloud sql import sql instance3 gs://sqlservermedia/WideWorldImporters-Full.bak.bak --database=db1 --quiet
+     
+    EOT
+}
+}
