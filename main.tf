faa373c95b95222e8299467b7d0e68c6118e85a3
subraban <136383571+subraban@users.noreply.github.com>
2023-07-08 15:07:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,26 @@
-
-
 resource "google_sql_database_instance" "sql_instance" {
-  name             = "mssqlinstance"
-  database_version = "SQLSERVER_2019_EXPRESS"
-  region           = "us-central1"
-  project          = "groovy-karma-388506"
+  name             = "instance1"
+  database_version = "SQLSERVER_2017_EXPRESS"
+   # ... Other configuration options for your SQL instance ...
+}
 
-  settings {
-    tier = "db-n1-standard-2"
+resource "google_sql_database" "sql_database" {
+  name   = "db4"
+  instance = google_sql_database_instance.sql_instance.name
+}
+
+resource "google_storage_bucket" "backup_bucket" {
+  name     = "sqlservermedia"
+}
+
+resource "null_resource" "import_script" {
+  depends_on = [google_sql_database_instance.sql_instance]
+
+  provisioner "local-exec" {
+    command = <<EOF
+gcloud sql import sqlserver "${google_sql_database_instance.sql_instance.name}" \
+  "gs://${google_storage_bucket.backup_bucket.name}/WideWorldImporters-Full.bak" \
+  --database="${google_sql_database.sql_database.name}"
+EOF
   }
-}
-
-resource "google_storage_bucket_object" "backup_object" {
-  name       = "WideWorldImporters-Full.bak"
-  bucket     = "sqlservermedia"
-  source     = "https://storage.cloud.google.com/sqlservermedia/WideWorldImporters-Full.bak" 
-  
-}
-
-resource "google_sql_database_instance_import" "import" {
-  name       = "import"
- 
-  instance   = google_sql_database_instance.sql_instance.name
-  database   = "db3"
-  uri        = "gs://$sqlservermedia/WideWorldImporters-Full.bak"
 }
